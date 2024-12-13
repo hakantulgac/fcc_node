c36e111402ad209e4e6b16e0ed8b047f03d3a9dd
hakantulgac <hakantulgac@gmail.com>
2024-12-13 19:55:55 +0300

    add req query

--- myApp.js
+++ myApp.js
@@ -32,6 +32,9 @@ app.get("/:word/echo", (req, res)=>{
     res.json({echo: req.params.word});
 });
 
+app.route("/name").get((req, res)=>{
+    res.json({ name: req.query.first+" "+req.query.last});
+}).post()
 
 
 
