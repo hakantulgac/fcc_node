320b9a8c083191f8a0104dd0247f249b6fb48b35
hakantulgac <hakantulgac@gmail.com>
2024-12-21 00:30:31 +0300

    add api/users operations

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+const {MongoClient} = require("mongodb");
+const client = new MongoClient(process.env.DB_URL);
+
+const db = client.db("test");
+const people = db.collection("people");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+app.route('/api/users')
+    .get( async (req, res) => {
+      const result = await people.find().toArray();
+      res.send(result);
+    })
+    .post( async (req, res) => {
+      const {username} = req.body;
+      const {insertedId} = await people.insertOne({username});
+    
+      res.json({username, _id: insertedId})
+    })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
